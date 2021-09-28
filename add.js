7a2877e30bb721992d63a841e512b79df4560dd9
devanshirocks10 <devanshirocks10@gmail.com>
2021-09-28 19:39:43 +0530

    add

---
+++ add.js
@@ -0,0 +1,11 @@
+var a=2
+var b=10
+console.log('a+b=',a+b)
+console.log('a-b=',a-b)
+console.log('b/a=',b/a)
+console.log('b**a=' ,b**a)
+var c=10
+console.log(5&&6)
+console.log(7&&8)
+console.log(8&&9)
+console.log(10&&11)
